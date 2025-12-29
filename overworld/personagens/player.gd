8f2d498cbdc1279d52b7ad943f460f881238fe82
Ultramarinemon <ultramarinemaan@gmail.com>
2025-12-28 22:28:44 -0300

    Tilemap e colisão
    Teste de tilemap e sistema de colisão adicionados, e troca dos placeholders

---
+++ overworld/personagens/player.gd
@@ -0,0 +1,35 @@
+extends CharacterBody2D
+
+var input_dir
+var tile_size = 32
+var moving = false
+
+func _physics_process(delta: float) -> void:
+	input_dir = Vector2.ZERO
+	if Input.is_action_pressed("BaixoQWERTY"):
+		if !$ray_baixo.is_colliding():
+			input_dir = Vector2(0, 1)
+			move()
+	elif Input.is_action_pressed("CimaQWERTY"):
+		if !$ray_cima.is_colliding():
+			input_dir = Vector2(0, -1)
+			move()
+	elif Input.is_action_pressed("EsquerdaQWERTY"):
+		if !$ray_esquerda.is_colliding():
+			input_dir = Vector2(-1, 0)
+			move()
+	elif Input.is_action_pressed("DireitaQWERTY"):
+		if !$ray_direita.is_colliding():
+			input_dir = Vector2(1, 0)
+			move()
+
+func move():
+	if input_dir:
+		if moving == false:
+			moving = true
+			var tween = create_tween()
+			tween.tween_property(self, "position", position + input_dir*tile_size, 0.35)
+			tween.tween_callback(move_false)
+			
+func move_false():
+	moving = false
